7e59c67141e089ed7fdd9708d4e2ed87b9930754
seongeun223 <jklun080921@gmail.com>
2024-05-10 11:12:12 +0900

    출력 문자 변경

--- src/main/java/oghiraffers/example.java
+++ src/main/java/oghiraffers/example.java
@@ -3,6 +3,6 @@ package oghiraffers;
 public class example {
     public static void main(String[] args) {
         System.out.print("hello World");
-System.out.print("한글은 깨져요");
+System.out.print("배고프다");
 }
 }
